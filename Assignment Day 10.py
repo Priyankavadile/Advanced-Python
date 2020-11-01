a541e798ffe13c1aacb8b58e18ce2db493d46993
Priyanka Wadile <48831612+Priyankavadile@users.noreply.github.com>
2020-11-01 17:43:58 +0530

    Add files via upload

---
+++ Assignment Day 10.py
@@ -0,0 +1,8 @@
+# ## Assignment
+# Convert 15-08-2020 15:20:20 into epoch time
+
+import time
+t1='15-08-2020 15:20:20'
+t9=time.strptime(t1,'%d-%m-%Y %H:%M:%S')
+epoch=time.mktime(t9)
+print('The epoch time for 15-08-2020 15:20:20 is: ',epoch)
